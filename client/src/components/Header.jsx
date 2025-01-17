8c067e16acb5f51b600c5b47b561b021a6291b9d
HarmonyMorse <harmonythedev@gmail.com>
2025-01-17 10:38:07 -0800

    Add Edit Persona button to user status menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -324,6 +324,14 @@ function Header({ onLogout = () => { } }) {
                                         )}
                                         <div className="border-t border-gray-100 my-1" />
                                         <button
+                                            className="w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 flex items-center"
+                                        >
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                            </svg>
+                                            Edit Persona
+                                        </button>
+                                        <button
                                             onClick={onLogout}
                                             className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
                                         >
